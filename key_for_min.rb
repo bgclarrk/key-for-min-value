cfc471c652828f8387492baa64d4d894bd64e737
+ <+>
2021-03-27 14:25:34 -0700

    commit tree caf28ea7ee443f55ca72a1601fbc5b77059a4355

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
 
     name_hash.each do |key, value|
         if value < default_value
-            default_value = value if value
+            default_value = value
             smallest_value_key = key
         end
     end
